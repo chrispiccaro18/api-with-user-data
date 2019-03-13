6c66fc9de895802a79d89850bff063c0e5c8cbc1
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-13 15:33:50 -0700

    search bar ddd-ed,

--- test/card-component.test.js
+++ test/card-component.test.js
@@ -12,7 +12,7 @@ test('make li for card list', assert => {
             <h2>Orbital ATK CRS-7 Liftoff</h2>
             <img src="https://images-assets.nasa.gov/image/KSC-20170418-PH_AWG06_0015/KSC-20170418-PH_AWG06_0015~thumb.jpg"
             alt="Image of Orbital ATK CRS-7 Liftoff">
-            <p>With blue sky for a background, the Orbital ATK Cygnus pressurized cargo module is carried atop the United Launch Alliance Atlas V rocket from Space Launch Complex 41 at Cape Canaveral Air Force Station in Florida. Orbital ATK\'s seventh commercial resupply services mission, CRS-7, will deliver 7,600 pounds of supplies, equipment and scientific research materials to the International Space Station. Liftoff was at 11:11 a.m. EDT. </p>
+            <p>With blue sky for a background, the Orbital ATK Cygnus pressurized cargo module is carried atop the United Launch Alliance Atlas V rocket from Space Launch Complex 41 at Cape Canaveral Air Force Station in Florida. Orbital ATK's seventh commercial resupply services mission, CRS-7, will deliver 7,600 pounds of supplies, equipment and scientific research materials to the International Space Station. Liftoff was at 11:11 a.m. EDT. </p>
         </li>
     `;
 
