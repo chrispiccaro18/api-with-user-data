1f53a3c06158f3da28688a7ff94f56f92d621a42
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-13 15:04:33 -0700

    Card component test passing as dom

--- test/card-component.test.js
+++ test/card-component.test.js
@@ -3,13 +3,16 @@ const test = QUnit.test;
 QUnit.module('Card constructor');
 
 function constructCard(){
-    return  /*html*/ `
+    const html = /*html*/ `
         <li>
             <h2>LROC First Look at the Apollo Landing Sites</h2>
             <img src="https://images-assets.nasa.gov/image/PIA12889/PIA12889~thumb.jpg" alt="Image of LROC First Look at the Apollo Landing Sites">
             <p>NASA Lunar Reconnaissance Orbiter first look at the Apollo landing sites.</p>
         </li>
     `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
 }
 
 test('make li for card list', assert => {
@@ -26,5 +29,5 @@ test('make li for card list', assert => {
     const result = constructCard();
 
     // assert
-    assert.equal(result, expected);
+    assert.htmlEqual(result, expected);
 });
