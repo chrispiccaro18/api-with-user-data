12db3eec89f7c1be5fed3fca4fc2f6370ea8d2c1
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-14 11:01:57 -0700

    hidden toggle settings for paging/prompt did

--- README.md
+++ README.md
@@ -39,8 +39,8 @@ You will likely need to sign up for an API key.
 //make-url function
 //update query function (assembly function)
 //paging
-
 //make sure have a search term
+
 //hidden stuff (css classes, js func)
 //filtering?
 //year filter detail page?
--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ import updateSearchTerm from './search-component.js';
 import makeSearchURL from './make-search-url.js';
 import { readHashQuery } from './hash-query.js';
 import { updatePagingInfo } from './paging-component.js';
- 
+
+const promptSection = document.getElementById('prompt-section');
+const pagingSection = document.getElementById('paging-section');
+const listSection = document.getElementById('list-section');
+
 window.addEventListener('hashchange', () => {
     const existingQuery = window.location.hash.slice(1);
     const searchOptions = readHashQuery(existingQuery);
     updateSearchTerm(searchOptions.searchTerm);
     const url = makeSearchURL(searchOptions);
     if(!url) {
+        promptSection.classList.remove('hidden');
+        pagingSection.classList.add('hidden');
+        listSection.classList.add('hidden');
         return;
+    } else {
+        promptSection.classList.add('hidden');
+        pagingSection.classList.remove('hidden');
+        listSection.classList.remove('hidden');
+        fetch(url)
+            .then(response => response.json())
+            .then(result => {
+                loadGallery(result.collection.items);
+                updatePagingInfo(result.collection.metadata);
+            });
     }
-    fetch(url)
-        .then(response => response.json())
-        .then(result => {
-            loadGallery(result.collection.items);
-            updatePagingInfo(result.collection.metadata);
-        });
 });
