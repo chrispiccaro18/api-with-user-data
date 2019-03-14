67974e3549ac7e24d6ee5a1350795e843607f9ad
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-13 19:16:04 -0700

    Loading gallery from fetch

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import loadGallery from './card-component.js';
-import data from '../data/items.js';
 import updateSearchTerm from './search-component.js';
 import makeSearchURL from './make-search-url.js';
+import { readHashQuery } from './hash-query.js';
  
 window.addEventListener('hashchange', () => {
-    const searchOptions = window.location.hash.slice(1);
+    const existingQuery = window.location.hash.slice(1);
+    const searchOptions = readHashQuery(existingQuery);
+    updateSearchTerm(searchOptions.searchTerm);
     const url = makeSearchURL(searchOptions);
     fetch(url)
         .then(response => response.json())
         .then(result => {
-            console.log(result.items);
+            loadGallery(result.collection.items);
         });
 });
-loadGallery(data);
-
