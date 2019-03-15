235aabc50ef833e2d136458a4c671abd3c44d258
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-15 09:30:49 -0700

    Started favorites page

---
+++ src/favorites/favorites.js
@@ -0,0 +1,16 @@
+import loadHeader from '../shared/header-component.js';
+import { auth, favoritesByUserRef } from '../firebase.js';
+import loadGallery from '../components/card-component.js';
+
+loadHeader();
+
+auth.onAuthStateChanged(user => {
+    const userId = user.uid;
+    const userFavoritesRef = favoritesByUserRef.child(userId);
+
+    userFavoritesRef.once('value')
+        .then(snapshot => {
+            const favoriteImagesAsObject = snapshot.val();
+            
+        });
+});
